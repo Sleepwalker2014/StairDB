02beaa598c174a9d24356d088c9cb86d9152b02a
Marcel Roa <mroa@limetec-biotechnologies.de>
2017-01-18 12:24:57 +0100

    add console file

---
+++ php/console/stairdb.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcel
+ * Date: 18.01.17
+ * Time: 12:21
+ */
+
+showApplicationInfo();
+
+function showApplicationInfo () {
+    echo "hi".PHP_EOL;
+}
